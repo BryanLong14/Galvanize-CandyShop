f718e6cb920a34cec69d4356413df618d90b3d83
Bryan Long <BryanLong1@gmail.com>
2017-11-13 10:54:20 -0700

    Candy Shop 1 Refactored

--- drills/version-1/exercise.js
+++ drills/version-1/exercise.js
@@ -1,14 +1,15 @@
 
+var candySalesDay = {};
+var sale = {};
 
-function candySalesToObject(arr) {
-   var candySalesToObject = arr.reduce (test,{});
-function test(sale, thing, index) {
+function reduceFun(sale, thing, index) {
   switch (index) {
     case 0:
       sale.item = thing;
       break;
     case 1:
       sale.price = thing;
+      break;
     case 2:
       sale.quantity = thing;
       break;
@@ -17,84 +18,67 @@ function test(sale, thing, index) {
   }
   return sale;
 }
+
+function mapFunc(thing, index) {
+  switch (index) {
+    case 0:
+      sale.item = thing;
+      break;
+    case 1:
+      sale.price = thing;
+      break;
+    case 2:
+      sale.quantity = thing;
+      break;
+    default:
+     return;
+  }
+  return sale;
+}
+
+
+// Test 1
+function candySalesToObject(arr) {
+   var candySalesToObject = arr.reduce (reduceFun,{});
 return candySalesToObject;
 }
 
 // Test 2
-
-var candySalesOldObject =
-{
-    "2015-01-06": [
-        ["Dark Chocolate Crunchies", "4.39", 1],
-        ["Mint Wafers", "1.19", 3]
-    ]
+function reducePushFun(item, thing) {
+  candySalesDay.sales.push(thing.reduce(reduceFun, {}));
 }
 
 function salesDayToObject(date, obj) {
-  var candySalesDay = {};
   candySalesDay.date = date;
   candySalesDay.sales = [];
-
-  for (var i = 0; i < obj[date].length; i++) {
-     candySalesDay.sales.push(obj[date][i].reduce(reduceFun, {}));
-     }
-
-  function reduceFun(sale, thing, index) {
-    switch (index) {
-      case 0:
-        sale.item = thing;
-        break;
-      case 1:
-        sale.price = thing;
-      case 2:
-        sale.quantity = thing;
-        break;
-      default:
-       return;
-    }
-    return sale;
-  }
+  obj[date].reduce(reducePushFun, {});
   return candySalesDay;
 }
 
-console.log(salesDayToObject("2015-01-06", candySalesOldObject));
-
-
+// Test 3
 function allSalesToArray(object) {
   var newSalesArray = [];
   var saleDates = Object.keys(object);
-  var tempObject = {};
-  for (var i = 0; i < saleDates.length; i++) {
-    tempObject = {
-      date: saleDates[i],
+  saleDates.map(mapFun);
+
+  function mapFun(thing, index) {
+    sale = {
+      date: saleDates[index],
       sales: []
     };
-    newSalesArray.push(tempObject);
-  }
+    newSalesArray.push(sale);
+  };
+
+
   for (var j = 0; j < newSalesArray.length; j++) {
     for (var k = 0; k < object[saleDates[j]].length; k++) {
-      tempObject = {};
-      object[saleDates[j]][k].map(function(thing, index) {
-        switch (index) {
-          case 0:
-            tempObject.item = thing;
-            break;
-          case 1:
-            tempObject.price = thing;
-            break;
-          case 2:
-            tempObject.quantity = thing;
-            break;
-          default:
-            return tempObj;
-        }
-      });
-      newSalesArray[j].sales.push(tempObject)
+      sale = {};
+      object[saleDates[j]][k].map(mapFunc);
+      newSalesArray[j].sales.push(sale)
     }
   }
   return newSalesArray;
 }
-
 
 module.exports = {
     // Uncomment these functions as you write them
